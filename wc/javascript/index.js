edc36e5b6bb6934fc1ee91419b45d174a12737ac
soxg <jdgray0224@gmail.com>
2023-08-28 15:51:40 -0400

    complete javascript wctool

--- wc/javascript/index.js
+++ wc/javascript/index.js
@@ -1,50 +1,56 @@
-const fs = require('fs')
-const path = require('path')
+const fs = require('fs');
+const path = require('path');
 
 const args = process.argv.slice(2);
-
-let option = args[0]
+let option = args[0];
 let file;
 
 if (!option.startsWith('-')) {
-    file = (__dirname, option)
-    option = 'default'
+    file = path.join(__dirname, option);
+    option = 'default';
 } else {
-    file = path.join(__dirname, args[1]) 
+    file = args[1] ? path.join(__dirname, args[1]) : null; 
 }
 
+function ccwc(text) {
+    let lines = text.split('\n').filter((ele) => ele !== '').length;
+    let words = text.split(/\s+/).filter(Boolean).length;
+    let bytes = Buffer.from(text).length;
+    let chars = text.length;
 
-async function ccwc(){
-    try{
-        const text = fs.readFileSync(file, 'utf-8');
-        let lines = text.split('\n').filter((ele) => ele !== '').length
-        let words = text.split(/\s+/).filter(Boolean).length
-        let bytes = Buffer.from(text).length
-        let chars = text.length
-
-        console.log(option);
-        switch (option) {
-            case "-l": // number of lines
-                // console.log('yes')
-                console.log(lines)
-                break;
-            case "-c": //number of bytes
-                console.log(bytes)
-                break;
-            case "-w": // number of words
-                console.log(words)
-                break;
-            case " -m": // number of characters
-                console.log(chars)
-                break;
-            case "default":
-                console.log(lines, words, bytes)
-                break;
-        }
-
-    } catch (error) {
-        console.log('Error reading:', error)
+    switch (option) {
+        case "-l": 
+            console.log(lines);
+            break;
+        case "-c": 
+            console.log(bytes);
+            break;
+        case "-w": 
+            console.log(words);
+            break;
+        case "-m": 
+            console.log(chars);
+            break;
+        case "default":
+        default:
+            console.log(lines, words, bytes);
+            break;
     }
-    
 }
-ccwc()
+
+if (!file) {
+    let inputData = '';
+    process.stdin.on('data', chunk => {
+        inputData += chunk;
+    });
+    process.stdin.on('end', () => {
+        ccwc(inputData);
+    });
+} else {
+    try {
+        const text = fs.readFileSync(file, 'utf-8');
+        ccwc(text);
+    } catch (error) {
+        console.log('Error reading:', error);
+    }
+}
